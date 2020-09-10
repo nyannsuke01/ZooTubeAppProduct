874fc8a32bae443d043a523d15ea4cf5a6b207e4
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-09-11 01:26:41 +0900

    MyPage CollectionView実装前

--- YoutubeApp/ViewControllers/MyPage/MyPageViewController.swift
+++ YoutubeApp/ViewControllers/MyPage/MyPageViewController.swift
@@ -10,6 +10,114 @@ import UIKit
 
 class MyPageViewController: UIViewController {
 
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var likeAnimalLabel: UILabel!
+    @IBOutlet weak var changeIconButton: UIButton!
+    @IBOutlet weak var videoListCollectionView: UICollectionView!
+
+    private var prevContentOffset: CGPoint = .init(x: 0, y: 0)
+    private let headerMoveHeight: CGFloat = 5
+
+    private let cellId = "cellId"
+    private let atentionCellId = "atentionCellId"
+    private var videoItems = [Item]()
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+        fetchYoutubeSerachInfo()
+    }
+
+    private func setupViews() {
+        videoListCollectionView.delegate = self
+        videoListCollectionView.dataSource = self
+
+        // VideoListCellのコレクションビューを設定
+        videoListCollectionView.register(UINib(nibName: "VideoListCell", bundle: nil), forCellWithReuseIdentifier: cellId)
+        videoListCollectionView.register(AttentionCell.self, forCellWithReuseIdentifier: atentionCellId)
+        //プロフィール写真を円に設定
+        iconImageView.layer.cornerRadius = 20
+
+    }
+    //Youtube検索情報を取得
+    private func fetchYoutubeSerachInfo() {
+        let params = ["q": "ねこ　かわいい"]
+
+        API.shared.request(path: .search, params: params, type: Video.self) { (video) in
+            self.videoItems = video.items
+            let id = self.videoItems[0].snippet.channelId
+            self.fetchYoutubeChannelInfo(id: id)
+        }
+    }
+
+    //Youtubeチャンネル情報を取得
+    private func fetchYoutubeChannelInfo(id: String) {
+        let params = [
+            "id": id
+        ]
+
+        API.shared.request(path: .channels, params: params, type: Channel.self) { (channel) in
+            self.videoItems.forEach { (item) in
+                item.channel = channel
+            }
+
+            self.videoListCollectionView.reloadData()
+        }
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+extension MyPageViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    //　videoListCollectionViewのCollectionViewの処理
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        let videoViewController = UIStoryboard(name: "Video", bundle: nil).instantiateViewController(identifier: "VideoViewController") as VideoViewController
+
+        videoViewController.selectedItem = indexPath.row > 2 ? videoItems[indexPath.row - 1] : videoItems[indexPath.row]
+
+        self.present(videoViewController, animated: true, completion: nil)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.view.frame.width
+
+        if indexPath.row == 2 {
+            return .init(width: width, height: 200)
+        } else {
+            return .init(width: width, height: width)
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return videoItems.count + 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == 2 {
+            let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: atentionCellId, for: indexPath) as! AttentionCell
+            cell.videoItems = self.videoItems
+
+            return cell
+        } else {
+            let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! VideoListCell
+
+            if self.videoItems.count == 0 { return cell }
+
+            if indexPath.row > 2 {
+                cell.videoItem = videoItems[indexPath.row - 1]
+            } else {
+                cell.videoItem = videoItems[indexPath.row]
+            }
+
+            return cell
+        }
+    }
+
 }
 
 
